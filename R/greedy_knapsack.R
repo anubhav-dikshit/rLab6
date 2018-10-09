b7611de49e6f5ab8b22bb88186d00b0e57e1a56c
Anubhav Dikshit <anubhav.dikshit@live.in>
2018-10-09 22:00:31 +0200

    Added greedy knapsack

---
+++ R/greedy_knapsack.R
@@ -0,0 +1,16 @@
+#' Title Greedy Algorithm Knapsack
+#'
+#' @param x The input to function containing the number of items
+#' @param W The weights of the items
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples set.seed(42)
+#' n <- 2000
+#' knapsack_objects <- data.frame(w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000))
+#'greedy_knapsack(x = knapsack_objects[1:8,], W = 3500)
+#'
+greedy_knapsack <- function(x, W){
+
+}
